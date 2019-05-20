95e2f24f81a541ab3fbb50d279bd265671616058
Shawn You <895864752@qq.com>
2019-05-20 15:33:52 +0800

    the description of the need

--- src/overview/draw.js
+++ src/overview/draw.js
@@ -1,3 +1,17 @@
-(function(global, doc){
+// draw.js is a script that draw the file relation,
+// two column and the middle is relation,
+// the right column is the left's direction,
+// the middle line thickness indicates that both files have a high relation,
+(function(global, doc, utils){
+    // the depending file relation data
     console.log(global.data);
-})(window, document);
+
+})(window, document, (function(){
+    // some util methods
+
+
+
+    return {
+
+    };
+})());
