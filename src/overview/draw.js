41e0b0d6658285e173ed6ed643cfa683ae98ab05
Shawn You <895864752@qq.com>
2019-05-24 18:19:28 +0800

    draw relation diagram

--- src/overview/draw.js
+++ src/overview/draw.js
@@ -9,11 +9,13 @@
 console.log(global.data);
 let cloneTreeData = JSON.parse(JSON.stringify(global.data));
 let mapTree = {};
+let showDependingNode = {};
+let showCurrentNode = {};
 
 // basic most common variable
-let dependingTreeDom = document.querySelector('.depending_tree');
-let currentTreeDom = document.querySelector('.current_tree');
-let mapLinesDom = document.querySelector('.lines');
+let dependingTreeDom = doc.querySelector('.depending_tree');
+let currentTreeDom = doc.querySelector('.current_tree');
+let mapLinesDom = doc.querySelector('.lines');
 
 main();
 
@@ -23,26 +25,76 @@ function main() {
     // handle tree data
     treeDataInit(cloneTreeData, mapTree);
 
-    console.log(cloneTreeData);
-
-    render();
-}
-
-// render file and folder tree and svg link lines
-function render(){
-
     // render depending folder and file tree
     renderTree(dependingTreeDom);
 
     // render current folder and file tree
     renderTree(currentTreeDom);
+    console.log(cloneTreeData);
 
-    drawLines();
+    // calculate depending weight
+    calDepsWeight(showDependingNode, showCurrentNode);
+
+    drawLines(showDependingNode, showCurrentNode);
 }
+
+// calculate weight based on currently showing node
+// @dependNodes {Map} id -> {}
+// @currentNodes {Map} id ->  {}
+function calDepsWeight(dependNodes, currentNodes) {
+    let dependNodeIds = Object.keys(dependNodes);
+    let currentNodeIds = Object.keys(currentNodes);
+
+    dependNodeIds.forEach((id) => {
+        // first of all ,solve the first folder
+        let firstNode = mapTree[id];
+
+        // map to current node and give a weight
+        dependNodes[id] = {};
+        loopData(firstNode, (childNode) => {
+            if (Array.isArray(childNode.deps)) {
+                childNode.deps.forEach((subId) => {
+                    // find current node id and  add weight
+                    let referringToCurrentNodeId = findMapId(mapTree[subId],  currentNodeIds);
+                    if (referringToCurrentNodeId) {
+                        if (!dependNodes[id][referringToCurrentNodeId]) {
+                            dependNodes[id][referringToCurrentNodeId] = 1;
+                        } else {
+                            dependNodes[id][referringToCurrentNodeId] += 1;
+                        }
+                    }
+                })
+            }
+        });
+    });
+}
+
+// find map id
+// @dependingNode {String}
+// @showCurrentNode {Array} all currently showing node ids
+function findMapId(depNode, showCurrentIds) {
+    let id;
+    recursiveParent(depNode, (parentNode) => {
+        if ( parentNode && showCurrentIds.includes(parentNode.id)) {
+            id = parentNode.id;
+        }
+    });
+    return id;
+}
+
+// recursion parent
+function recursiveParent(node, callback) {
+    callback && callback(node);
+
+    if (node && node.parent) {
+        recursiveParent(node.parent, callback);
+    }
+}
+
 
 // render current file and folder tree
 function renderTree(rootDom) {
-    let ulDom = document.createElement('ul');
+    let ulDom = doc.createElement('ul');
     let treeDom = recursiveDomTree(cloneTreeData, ulDom);
     rootDom.appendChild(treeDom);
 }
@@ -57,6 +109,10 @@ function recursiveDomTree(cloneTreeData, dom){
         let childDom = appendElement(dom, 'li', {});
         childDom.setAttribute('data-id', node.id);
         childDom.innerHTML = `<img src="./images/file.png" />${node.fName}`;
+        if (node.level === 0) {
+            showDependingNode[node.id] = {};
+            showCurrentNode[node.id] = {};
+        }
 
         if (Array.isArray(node.leaves) && node.leaves.length > 0) {
             childDom.innerHTML = `<img src="./images/folder.png" />${node.fName}`;
@@ -74,7 +130,7 @@ function recursiveDomTree(cloneTreeData, dom){
 // @styleObj {Object} css style object
 // @return {DOM} new ul dom
 function appendElement (parentDom, ele, styleObj) {
-    let element = document.createElement(ele);
+    let element = doc.createElement(ele);
 
     if (styleObj) {
         let cssText = '';
@@ -88,7 +144,30 @@ function appendElement (parentDom, ele, styleObj) {
 }
 
 // draw lines to link from depending tree to current tree
-function drawLines(){
+function drawLines(showDependingNode){
+
+    let  draw = SVG('drawing').size(currentTreeDom.clientWidth, currentTreeDom.clientHeight);
+    Object.keys(showDependingNode).forEach((id)=>{
+        let oneNode = showDependingNode[id];
+
+        let depDom = document.querySelector('.depending_tree  [data-id="'+ id +'"]');
+        let startPosition = {
+            x: 0,
+            y: depDom.offsetTop + 15 - 10,
+        };
+
+        Object.keys(oneNode).forEach((refId, index) => {
+            let bolderStroke = Math.sqrt(oneNode[refId]);
+            let dom = document.querySelector('.current_tree [data-id="'+ refId +'"]');
+            let endPosition = {
+                x: currentTreeDom.clientWidth,
+                y: dom.offsetTop + 15 - 10,
+            }
+            console.log(mapTree[refId], dom, endPosition);
+            draw.polyline([[0, startPosition.y], [endPosition.x, endPosition.y]])
+            .stroke({ width:  bolderStroke})
+        });
+    });
 
 }
 
@@ -113,13 +192,13 @@ function treeDataInit(cloneTreeData, mapTree) {
         if (Array.isArray(node.leaves)) {
             node.type = 'closed';
         }
-    });
+    }, 0);
 }
 
 // loop every node by recursion
 // @treeData {Object} it is a tree
 // @callback {Function} a callback function
-function loopData(treeData, callback ) {
+function loopData(treeData, callback , level) {
 
     if (!treeData) {
         return ;
@@ -127,9 +206,12 @@ function loopData(treeData, callback ) {
 
     if (Array.isArray(treeData.leaves)) {
         treeData.leaves.forEach((child) => {
+            if ( Number.isFinite(level) ) {
+                child.level = level;
+            }
             callback && callback(child, treeData);
             if (child.leaves) {
-                loopData(child, callback);
+                loopData(child, callback, level + 1);
             }
         });
     } else {
